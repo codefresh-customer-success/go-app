803f658d0a5dc1cf9a31c94eec686e04338e1fbd
francisco-codefresh <francisco@codefresh.io>
2021-10-02 13:40:10 -0600

    Adding Random generator function

--- src/sample/trivial-web-server.go
+++ src/sample/trivial-web-server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"math/rand"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -11,12 +12,19 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "OK!")
+	fmt.Fprintf(w, "OK")
+}
+
+func randomHandler(w http.ResponseWriter, r *http.Request) {
+    min := 0
+    max := 100
+	fmt.Fprintf(w, rand.Intn(max - min) + min)
 }
 
 func main() {
 	fmt.Println("Basic web server is starting on port 8080...")
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/healthz", healthzHandler)
+	http.HandleFunc("/random", randomHandler)
 	http.ListenAndServe(":8080", nil)
 }
